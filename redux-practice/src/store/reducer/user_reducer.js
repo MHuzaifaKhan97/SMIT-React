61204e19b3ab23494ba3a6dcfdc3d73c68ec1661
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2020-10-27 19:58:55 +0500

    Redux Practice

---
+++ redux-practice/src/store/reducer/user_reducer.js
@@ -0,0 +1,17 @@
+import { act } from "react-dom/test-utils"
+
+const INITIAL_STATE = {
+    name: 'Huzaifa Khan',
+    email: 'huzaifa@gmail.com'
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_DATA': return {
+            ...state,
+            name: action.payload.name,
+            email: action.payload.email,
+        }
+        default: return state;
+    }
+} 
